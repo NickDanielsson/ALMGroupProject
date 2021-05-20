9ea438e4598be3216372fa36b2ce83fc5ff4600e
Nick Danielsson <nickdanielsson87@gmail.com>
2021-05-20 16:11:05 +0200

    added tests to methods: isNotSick & pickUpChild

--- src/test/java/com/example/almgroupproject/Service/ChildServiceTest.java
+++ src/test/java/com/example/almgroupproject/Service/ChildServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.almgroupproject.Service;
 
+import com.example.almgroupproject.Models.Child;
 import com.example.almgroupproject.Repository.IChildRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class ChildServiceTest {
 
@@ -54,10 +57,42 @@ class ChildServiceTest {
     }
     // Nick
     @Test
+    @DisplayName("pickUpChild change isPresent to false")
     void pickUpChild() {
+
+        Child c = new Child("Alex", "Svensson");
+        c.setPresent(true);
+        System.out.println(c);
+
+        when(childRepository.findChildByFirstnameAndLastname(c.getFirstname(),c.getLastname()))
+                .thenReturn(c);
+
+        Child actual = cs.pickUpChild(c.getFirstname(),c.getLastname());
+        System.out.println(actual);
+
+        assertFalse(actual.isPresent());
+        assertEquals(c.getFirstname(),actual.getFirstname());
+        assertEquals(c.getLastname(),actual.getLastname());
+
+
     }
     // Nick
     @Test
+    @DisplayName("isSick changes to false when isNotSick is called")
     void isNotSick() {
+
+        Child c = new Child("Alice", "Svensson");
+        c.setSick(true);
+        System.out.println(c);
+
+        when(childRepository.findChildByFirstnameAndLastname(c.getFirstname(),c.getLastname()))
+                .thenReturn(c);
+
+        Child actual = cs.isNotSick(c.getFirstname(),c.getLastname());
+        System.out.println(actual);
+
+        assertFalse(actual.isPresent());
+        assertEquals(c.getFirstname(),actual.getFirstname());
+        assertEquals(c.getLastname(),actual.getLastname());
     }
 }
